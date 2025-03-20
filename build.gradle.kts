d9e88e9327f06dad8b17796413e1ab6d811a4809
vietviet08 <vietnq.23ceb@vku.udn.vn>
2025-03-20 12:03:38 +0700

    fix: 🐛 change version agp && kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ buildscript {
 
 
 plugins {
-    id("com.android.application") version "8.8.0" apply false
-    id("com.android.library") version "8.8.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.android.application") version "8.6.0-alpha07" apply false
+    id("com.android.library") version "8.6.0-alpha07" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
